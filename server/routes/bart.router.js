1ef25c33094716a8f8cb020ffd60c6fa04a2ac2e
Vada Karlen <vada.karlen@gmail.com>
2021-09-22 01:15:36 -0500

    Got webhook working

--- server/routes/bart.router.js
+++ server/routes/bart.router.js
@@ -45,4 +45,31 @@ router.post('/voice-input', (req, res) => {
   res.status(200).send({ data: 'VOICE ENDPOINT CONNECTION SUCCESSFUL' });
 });
 
+// router.get('/', (req, res) => {
+//   res.send('Server Is Working......');
+// });
+
+/**
+ * on this route dialogflow send the webhook request
+ * For the dialogflow we need POST Route.
+ * */
+router.post('/webhook', (req, res) => {
+  // get agent from request
+  let agent = new WebhookClient({ request: req, response: res });
+  console.log('REQ', req);
+
+  // create intentMap for handle intent
+  let intentMap = new Map();
+
+  // add intent map 2nd parameter pass function
+  intentMap.set('RandomFact', handleWebHookIntent);
+
+  // now agent is handle request and pass intent map
+  agent.handleRequest(intentMap);
+});
+
+function handleWebHookIntent(agent) {
+  agent.add('Hello I am Webhook demo How are you...');
+}
+
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const app = express();
 const bartRouter = require('./routes/bart.router');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /*** ROUTES ***/
 app.use('/api/bart', bartRouter);
